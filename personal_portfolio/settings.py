eeb5f70d34fbbd0f6ac3f86b0f84cd4202578dc1
EgorTolbaev <egor05.09.97@gmail.com>
2021-06-15 13:44:48 +0500

    add secrets

--- personal_portfolio/settings.py
+++ personal_portfolio/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from .secrets import *
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-0-yeml^w#ngt%#=9@lzuu&5l_d6y1(-vv6=1+$d7+mv-wm2xl4'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
